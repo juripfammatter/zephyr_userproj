68380bc3ffb4fe0b863296dec66fdfa0c1adc048
juripfammatter <juri.pfammatter@gmail.com>
2023-12-17 14:01:17 +0100

    1/4 step driver working

--- hardware_drivers/stepper_driver/src/main.c
+++ hardware_drivers/stepper_driver/src/main.c
@@ -21,59 +21,90 @@
 
 static struct gpio_dt_spec enable = GPIO_DT_SPEC_GET_BY_IDX(EN_DIR_NODE, gpios, 0);
 static struct gpio_dt_spec direction = GPIO_DT_SPEC_GET_BY_IDX(EN_DIR_NODE, gpios, 1);
+static struct gpio_dt_spec step = GPIO_DT_SPEC_GET_BY_IDX(EN_DIR_NODE, gpios, 2);
 
 void gpio_configure(const struct gpio_dt_spec gpio_spec, gpio_flags_t gpio_flag);
 
 /* PWM */
-static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
+// static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 
-#define MIN_PERIOD PWM_SEC(1U) / 128U
-#define MAX_PERIOD 31250000 //PWM_SEC(1U)
+// #define MIN_PERIOD PWM_SEC(1U) / 50U // 50Hz
+// #define MAX_PERIOD 31250000 //PWM_SEC(1U)
 
 int main(void)
 {
 	uint32_t pulse_width;
 	int ret;
 
-	printk("\nPWM-based stepper driver\n");
-	printk("Starting GPIO/PWM setup\n");
+	// printk("\nPWM-based stepper driver\n");
+	printk("Starting GPIO setup\n");
 
 	/* GPIO configuration */
 	gpio_configure(enable, GPIO_OUTPUT);
 	gpio_configure(direction, GPIO_OUTPUT);
+	gpio_configure(step, GPIO_OUTPUT);
 
-	/* PWM configuration */
-	if (!pwm_is_ready_dt(&pwm_led0)) {
-		printk("Error: PWM device %s is not ready\n",
-		       pwm_led0.dev->name);
-		return 0;
-	}
 
-	printk("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
-	       MAX_PERIOD, MIN_PERIOD);
 
-	pulse_width = MIN_PERIOD;
-	while (1) {
+	// /* PWM configuration */
+	// if (!pwm_is_ready_dt(&pwm_led0)) {
+	// 	printk("Error: PWM device %s is not ready\n",
+	// 	       pwm_led0.dev->name);
+	// 	return 0;
+	// }
+
+	// printk("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
+	//        MAX_PERIOD, MIN_PERIOD);
+
+	// set initial conditions
+	gpio_pin_set_dt(&enable, 0);
+	gpio_pin_set_dt(&direction, 0);
+	gpio_pin_set_dt(&step, 0);
+
+	// ret = pwm_set_dt(&pwm_led0, MIN_PERIOD, 0);
+	// if (ret) {
+	// 	printk("Error %d: failed to set pulse width\n", ret);
+	// 	return 0;
+	// }
+
+	k_sleep(K_MSEC(500));
+
+	// pulse_width = MIN_PERIOD/2;
+	// pulse_width = 0;
         
-		// set pulse width
-		ret = pwm_set_dt(&pwm_led0, MIN_PERIOD, pulse_width);
-		if (ret) {
-			printk("Error %d: failed to set pulse width\n", ret);
-			return 0;
-		}
+	gpio_pin_set_dt(&enable, 1);
+	gpio_pin_set_dt(&direction, 1);
 
-		// fade LED
-		// pulse_width -= period/100;
-		// printk("pulse width: %d\n", pulse_width);
+	k_sleep(K_MSEC(500));
 
-		// if(pulse_width <= period/100){
-		// 	pulse_width = period;
-		// }
-
-		// k_sleep(K_MSEC(30U));
-
-		k_sleep(K_MSEC(500));
+	// rotate
+	int i=0;
+	for(i=0; i<(200*4); i++){
+		gpio_pin_set_dt(&step, 1);
+		k_sleep(K_USEC(250/4));
+		gpio_pin_set_dt(&step, 0);
+		k_sleep(K_USEC(250/4));
 	}
+	// set pulse width
+	// ret = pwm_set_dt(&pwm_led0, MIN_PERIOD, pulse_width);
+	// if (ret) {
+	// 	printk("Error %d: failed to set pulse width\n", ret);
+	// 	return 0;
+	// }
+
+	// k_sleep(K_MSEC(5000));
+	// set pulse width
+	// ret = pwm_set_dt(&pwm_led0, MIN_PERIOD, 0);
+	// if (ret) {
+	// 	printk("Error %d: failed to set pulse width\n", ret);
+	// 	return 0;
+	// }
+
+	k_sleep(K_MSEC(500));
+
+	gpio_pin_set_dt(&enable, 0);
+	gpio_pin_set_dt(&direction, 0);
+	
 	return 0;
 }
 
