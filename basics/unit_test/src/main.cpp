1afb0c87653cd95e57b0e797f47013d1ed8a9b32
juripfammatter <juri.pfammatter@gmail.com>
2023-12-21 21:37:18 +0100

    added basic working unit test

---
+++ basics/unit_test/src/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>    // booleans
+#include <zephyr/kernel.h>
+#include <autoconf.h>
+#include <vector>
+
+int main(void)
+{
+    std::vector<int> vec = {0, 1, 2};
+    vec.push_back(42);
+
+    for(auto e: vec){
+        printk("%i\n", e);
+    }
+ 
+	printf("Hello World! %s\n", CONFIG_BOARD);
+    int i = 0.0;
+    while(true){
+        printk("%s is working since %i seconds \n", CONFIG_BOARD, i);
+        i++;
+        k_msleep(1000);
+    }
+	return 0;
+}
+
