98b0ba926eb82e4245a42071a7b4d71018af3c07
juripfammatter <juri.pfammatter@gmail.com>
2024-07-27 19:56:46 +0200

    typo, added .idea to gitignore

--- templates/template/src/main.c
+++ templates/template/src/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdbool.h>    // booleans
+//#include <stdbool.h>    // booleans
 #include <zephyr/kernel.h>
 #include <autoconf.h>
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD);
-    int i = 0.0;
-    while(true){
+    int i = 0;
+    while(i<10){
         printk("%s is working since %i seconds \n", CONFIG_BOARD, i);
         i++;
         k_msleep(1000);
